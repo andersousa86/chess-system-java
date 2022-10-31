47fe2fe845622bc53e448e187a23b990edb93db8
andersousa86 <andersousa86@gmail.com>
2022-10-31 10:03:58 -0300

    Criou e implementou mais peças

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -5,32 +5,42 @@ import chess.pecas.Rei;
 import chess.pecas.Torre;
 
 public class ChessMatch {
-	
+
 	private Tabuleiro tabuleiro;
-	
+
 	public ChessMatch() {
 		tabuleiro = new Tabuleiro(8, 8);
 		iniciarSetup();
 	}
-	
-	public PecaChess[][] getPecas(){
+
+	public PecaChess[][] getPecas() {
 		PecaChess[][] mat = new PecaChess[tabuleiro.getLinhas()][tabuleiro.getColunas()];
-		for(int i=0; i<tabuleiro.getLinhas(); i++) {
-			for(int j=0; j<tabuleiro.getColunas(); j++) {
+		for (int i = 0; i < tabuleiro.getLinhas(); i++) {
+			for (int j = 0; j < tabuleiro.getColunas(); j++) {
 				mat[i][j] = (PecaChess) tabuleiro.peca(i, j);
 			}
 		}
 		return mat;
 	}
-	
+
 	private void colocarNovaPeca(char coluna, int linha, PecaChess peca) {
 		tabuleiro.coloarPeca(peca, new ChessPosicao(coluna, linha).toPosicao());
 	}
-	
+
 	private void iniciarSetup() {
-		colocarNovaPeca('b', 6, new Torre(tabuleiro, Cor.BRANCA));
-		colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETA));
-		colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCA));
+		colocarNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCA));
+		colocarNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCA));
+		colocarNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCA));
+		colocarNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCA));
+		colocarNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCA));
+		colocarNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCA));
+
+		colocarNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETA));
+		colocarNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETA));
+		colocarNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETA));
+		colocarNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETA));
+		colocarNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETA));
+		colocarNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETA));
 	}
 
 }
